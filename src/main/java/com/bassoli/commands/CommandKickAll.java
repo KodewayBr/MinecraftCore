54fa2cfcb61d39c765a9f7ca0ea5a2a9cc9c8f0a
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2021-10-03 23:12:47 -0300

    update

--- src/main/java/com/bassoli/commands/CommandKickAll.java
+++ src/main/java/com/bassoli/commands/CommandKickAll.java
@@ -1,6 +1,5 @@
 package com.bassoli.commands;
 
-import lombok.val;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import org.bukkit.Bukkit;
@@ -10,10 +9,11 @@ import org.bukkit.entity.Player;
 public class CommandKickAll {
 
     @Command(
-            name = "kickall"
+            name = "kickall",
+            permission = "minister.kickall"
     )
 
-    public void handleKickAll(Context<CommandSender> context) {
+    public void handleKickAll(Context<Player> context) {
         for (Player target : Bukkit.getServer().getOnlinePlayers()) {
             if (!target.hasPermission("minister.kickall"))
                 target.kickPlayer("§cVocê foi kickado do servidor, tente entrar novamente ou entramos em manutenção.");
