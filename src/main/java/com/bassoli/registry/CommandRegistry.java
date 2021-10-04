73f1ad496e655bb293e7b050b72f6b7478f778f1
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2021-10-04 01:07:15 -0300

    update

--- src/main/java/com/bassoli/registry/CommandRegistry.java
+++ src/main/java/com/bassoli/registry/CommandRegistry.java
@@ -6,17 +6,19 @@ import com.bassoli.Bassoli;
 import me.saiintbrisson.bukkit.command.BukkitFrame;
 import me.saiintbrisson.minecraft.command.message.MessageHolder;
 import me.saiintbrisson.minecraft.command.message.MessageType;
+import org.bukkit.configuration.file.FileConfiguration;
 
 @Data(staticConstructor = "of")
 public class CommandRegistry {
 
     private final Bassoli plugin;
+    FileConfiguration config = Bassoli.getInstance().getConfig();
 
     public void register() {
         BukkitFrame bukkitFrame = new BukkitFrame(plugin);
 
         MessageHolder message = bukkitFrame.getMessageHolder();
-        message.setMessage(MessageType.NO_PERMISSION, "§cVocê não tem permissão para isso.");
+        message.setMessage(MessageType.NO_PERMISSION, config.getString("Message.No_Permissions").replace("&", "§"));
         message.setMessage(MessageType.INCORRECT_USAGE, "§cUtlize: /{usage}");
         bukkitFrame.registerCommands(
                 new CommandFly(),
